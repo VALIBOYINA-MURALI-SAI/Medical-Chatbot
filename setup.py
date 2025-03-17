5af73011fc764c253ea5c5a284bef9a2ad227b66
VALIBOYINA MURALI SAI <mv8039@srmist.edu.in>
2025-03-17 18:50:53 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Murali sai V',
+    author_email= 'mv8039@srmist.edu.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
